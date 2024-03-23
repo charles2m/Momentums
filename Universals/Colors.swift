c267d0e5a96024e1bd0cd16fe8ddeba55d973790
charles2m <ch.mmartiniere@hotmail.com>
2024-03-23 19:52:48 +0100

    V0.3dscdsfs

--- Universals/Colors.swift
+++ Universals/Colors.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+
+struct ColorTheme {
+    
+    let secondaryYellow600 = Color("secondaryYellow600")
+    let neutral900 = Color("neutral900")
+    let primary800 = Color("primary800")
+    let neutral200 = Color("neutral200")
+}
--- Universals/FontManager.swift
+++ Universals/FontManager.swift
@@ -5,4 +5,20 @@
 //  Created by Charles Martin Martinière on 23/03/2024.
 //
 
+
 import Foundation
+import SwiftUI
+
+struct FontManager {
+    
+    struct Time_New_Roman {
+        
+        static let regular = "Times New Roman"
+        
+        
+    }
+    
+    
+    
+    
+}
